0ef08dddc52d1d0eb32eaebf2ec7aa8cb2a47b87
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-31 11:28:55 -0700

    code

--- server.js
+++ server.js
@@ -11,7 +11,32 @@ app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // res.render('index', { name: 'John Doe', age: 47 })
+  // res.render('index', {
+  //   person: {
+  //     name: 'John Doe',
+  //     age: 47
+  //   }
+  // })
+  // res.render('index', {
+  //   people: ['John Doe', 'Jane Doe', 'Jack Doe']
+  // })
+  // res.render('index', {
+  //   isDone: false
+  // })
+  // res.render('index', {
+  //   people: [
+  //     {
+  //       name: 'John Doe'
+  //     },
+  //     {
+  //       name: 'Jane Doe'
+  //     },
+  //     {
+  //       name: 'Jack Doe'
+  //     }
+  //   ]
+  // })
 })
 
 app.listen(3000)
